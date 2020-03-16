2f8b1d3bc7cf41ed3591bbdef6f218660aac2dce
hihijenny <jenny_nijhof@hotmail.com>
2020-03-16 10:24:57 +0100

    poging tot versturen formulier

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
 const express = require('express');
 const app = express();
 const ejs = require("ejs");
-// const mongo = require("mongodb");
-// const slug = require("slug");
-// const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
+const mongo = require("mongodb");
+const slug = require("slug");
 const port = 3000;
 
-// const data = [
-//   {
-//   id: "sjoerd-bindels",
-//   naam: "Sjoerd Bindels",
-//   leeftijd: "23",
-//   bio: "Hoi ik ben Sjoerd"
-// },
+require("dotenv").config()
 
-// {
-//   id: "tess-smit",
-//   naam: "Tess Smit",
-//   leeftijd: "19",
-//   bio: "Hoi ik ben Tess"
-// }, 
+let db = null;
+let url = "mongodb//" + process.env.DB_HOST + ":" + process.env.DB_PORT;
 
-// { 
-//   id: "sam-slotemaker",
-//   naam: "Sam Slotemaker",
-//   leeftijd: "19",
-//   bio: "Hoi ik ben Sam"
-// }  
-// ]
-
-// require("dotenv").config()
-
-// let db = null
-// let url = "mongodb//" + process.env.DB_HOST + ":" + process.env.DB_PORT
-
+//Array met de Images om uit te kiezen
 const choices = [ 
   "images/burrito.jpg", 
   "images/tacos.jpg",
@@ -43,43 +21,59 @@ const choices = [
   "images/pizza.jpeg"
  ]
 
+ //genereren van een random afbeelding uit de array
 const random = () => {
-  return Math.floor(Math.random() * choices.length)
-}
-
+  return Math.floor(Math.random() * choices.length) 
+};
+//toewijzen van een random img aan de variabele
 let image1 = random();
 let image2 = random(); 
 
-const images = {
-  id: "afbeeldingen",
+
+//object om data uit te halen en naar de ejs template te sturen
+const data = {
+  id: "profiel",
+  naam: "Jenny Nijhof",
   img1: choices[image1],
   img2: choices[image2]
-}
+};
 
+//zorgt er voor dat je nooit twee keer dezelfde img terug krijgt
 while (image1 === image2) {
   image1 = random()
-}
+};
 
-
+//log welke img er worden getoond
 console.log(image1);
 console.log(image2);
 
+
+
+//routes
 app
   .use(express.static("public"))
   .set("view engine", "ejs")
   .set("views", "view")
+  .use(bodyParser.urlencoded({extended: true}))
+  .post("/sendChoice", sendChoice)
+  .get ("/choice", choice)
 
-app.get("/list", (req, res) => {
-  res.render("list.ejs", {
-    data
+  
+//route
+function choice(req, res)  {
+  res.render("choice.ejs", {data})
+}
+
+
+function sendChoice(req, res) {
+  data.push({
+   food: req.body.food //require food uit body
   })
 
-});
+  res.redirect('/choice');
+};
 
-app.get("/choice", (req, res) => {
-  res.render("choice.ejs", {images})
-});
-
+//404 foutmelding
 app.use(function(req, res) {
   res.send("404: Page not found", 404);
 });
