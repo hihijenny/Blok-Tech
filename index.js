83660005ce05819b5193b43e5c336cc89cf5fcf0
hihijenny <jenny_nijhof@hotmail.com>
2020-04-03 22:02:29 +0200

    Changed array to array of objects

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 const ejs = require("ejs");
@@ -6,14 +5,14 @@ const bodyParser = require('body-parser');
 const mongo = require("mongodb");
 const port = 3000;
 
-
 require("dotenv").config()
 
 let collection = null;
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@blok-tech-ezc4c.mongodb.net/test?retryWrites=true&w=majority"
 const client = new MongoClient(uri, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 client.connect(function (err, client) {
@@ -24,50 +23,42 @@ client.connect(function (err, client) {
   collection = client.db("blok-tech").collection("sendChoice");
 })
 
-mongo.MongoClient.connect(uri, function (err, client){
-  if(err) throw errdb = client.db(process.nextTick.DB_)
-})
-
-
-//Array met de Images om uit te kiezen
-const choices = [ 
-  "images/burrito.jpg", 
-  "images/tacos.jpg",
-  "images/burger.jpg",
-  "images/hot-dog.jpg",
-  "images/pasta.jpg",
-  "images/pizza.jpg"
+//Array met de image objects om uit te kiezen
+const choices = [
+  { imageUrl: "images/burrito.jpg", name:"burrito" },
+  { imageUrl: "images/tacos.jpg", name:"tacos" },
+  { imageUrl: "images/burger.jpg", name:"burger" },
+  { imageUrl: "images/hot-dog.jpg", name:"hot-dog" },
+  { imageUrl: "images/pasta.jpg", name:"pasta" },
+  { imageUrl: "images/pizza.jpg", name:"pizza" }
  ]
 
- //genereren van een random afbeelding uit de array
-const random = () => {
-  return Math.floor(Math.random() * choices.length) 
-};
+//genereren van een random afbeelding uit de array
+const getImage = () => {
+  return choices[(Math.floor(Math.random() * Math.floor(choices.length)))]
+}
 
-//toewijzen van een random img aan de variabele
-let image1 = random();
-let image2 = random(); 
-let image3 = random(); 
+ //genereren van een random afbeelding uit de array
+const getImageSet = () => {
+  let image1 = getImage();
+  let image2 = getImage();
+
+  //zorgt er voor dat je nooit twee keer dezelfde img object terug krijgt
+  while (image1 === image2) {
+    image1 = getImage()
+  }
+
+  return [image1, image2]
+}
 
 //object om data uit te halen en naar de ejs template te sturen
 const data = {
   id: "profiel",
   naam: "Jenny Nijhof",
-  img1: choices[image1],
-  img2: choices[image2],
+  imageSet1: getImageSet(),
+  imageSet2: getImageSet(),
+  imageSet3: getImageSet()
 };
-
-//zorgt er voor dat je nooit twee keer dezelfde img terug krijgt
-while (image1 === image2) {
-  image1 = random()
-}
-
-
-//log welke img er worden getoond
-console.log(image1);
-console.log(image2);
-
-
 
 //routes
 app
@@ -77,8 +68,6 @@ app
   .use(bodyParser.urlencoded({extended: true}))
   .post("/sendChoice", sendChoice)
   .get ("/choice", choice)
-
-  
 
 function choice(req, res)  {
   res.render("choice.ejs", {data})
@@ -95,19 +84,14 @@ function sendChoice(req, res, next) {
     if (err) {
       next(err)
     } else {
-      res.redirect('/')
+      res.redirect('/choice')
     }
   }
 }
-
-   
- 
 
 //404 foutmelding
 app.use(function(req, res) {
   res.send("404: Page not found", 404);
 });
-
-
 
 app.listen(port, () => console.log(`app running on port: ${port}`));
